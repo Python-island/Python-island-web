db736d636b01a8ee64ff3749cf1b32d5dab05835
鸡哥 <15251865468@163.com>
2026-03-26 22:39:53 +0800

    feat: 添加网站图标

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="zh">
       <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
